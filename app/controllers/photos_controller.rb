4de2e8e731735fb05c9edf397941fede8a685d4c
alexbaker3 <alexbaker3@users.noreply.github.com>
2017-04-28 17:25:45 -0500

    work

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,45 @@
-class PhotosController < Application Controller
+class PhotosController < ApplicationController
 
 def index
+  @list_of_all_my_photos= Photo.all
+
   render ("photos/index.html.erb")
 end
+
+def new_form
+
+  render ("photos/new_form.html.erb")
+end
+
+def create_row
+  url= params[:da_source]
+  cap= params[:da_caption]
+  new_photo=Photo.new
+  new_photo.source = url
+  new_photo.caption = cap
+  new_photo.save
+  #render ("photos/create_row.html.erb")
+  redirect_to("/photos/#{new_photo.id}")
+end
+
+def show
+the_id = params[:da_id]
+@my_photo = Photo.find(the_id)
+
+  render ("photos/show.html.erb")
+end
+
+def edit_form
+  render ("photos/edit_form.html.erb")
+end
+
+def update_row
+  render ("photos/update_row.html.erb")
+end
+
+def destroy_row
+  render ("photos/destroy_row.html.erb")
+end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get("/photos", (:controller => "photos" , :action => "index"))
 
-get("/photos/new", (:controller => "photos" , :action => "index"))
+# CREAT
+get("/photos/new", {:controller => "photos" , :action => "new_form"})
+
+get("/create_photo", { :controller => "photos", :action => "create_row" })
+
+# READ
+get("/photos", { :controller => "photos", :action => "index" })
+get("/photos/:da_id", { :controller => "photos", :action => "show" })
+
+# UPDATE
+get("/photos/:la_id/edit", { :controller => "photos", :action => "edit_form" })
+get("/update_photo/:le_id", { :controller => "photos", :action => "update_row" })
+
+# DELETE
+get("/delete_photo/:da_id", { :controller => "photos", :action => "destroy_row" })
 
 end
